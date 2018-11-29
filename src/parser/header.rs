7d1d4dd502a63387b1e791aa3188d9a720d9bf80
Ilari Mäkimattila <ilari@auterion.com>
2018-11-29 11:35:26 +0100

    Seek into beginning before validating header

--- src/parser/header.rs
+++ src/parser/header.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
-use std::io::Read;
+use std::io::prelude::*;
+use std::io::SeekFrom;
 
 const HEADER_BYTES: [u8; 7] = [85, 76, 111, 103, 1, 18, 53];
 
@@ -19,6 +20,8 @@ impl ULogHeader for File {
     /// assert!(log_file.is_ulog());
     /// ```
     fn is_ulog(&mut self) -> bool {
+        self.seek(SeekFrom::Start(0))
+            .expect("File must be seekable");
         let mut buffer = [0; 7];
         if let Ok(bytes) = self.read(&mut buffer) {
             bytes == 7 && buffer == HEADER_BYTES
@@ -34,8 +37,22 @@ mod tests {
 
     #[test]
     fn it_does_not_validate_incorrect_file() {
-        let filename = format!("{}/tests/fixtures/not_a_log_file.txt", env!("CARGO_MANIFEST_DIR"));
+        let filename = format!(
+            "{}/tests/fixtures/not_a_log_file.txt",
+            env!("CARGO_MANIFEST_DIR")
+        );
         let mut log_file = std::fs::File::open(&filename).unwrap();
         assert!(!log_file.is_ulog());
+    }
+
+    #[test]
+    fn it_seeks_to_the_beginning_when_validating() {
+        let filename = format!(
+            "{}/tests/fixtures/6ba1abc7-b433-4029-b8f5-3b2bb12d3b6c.ulg",
+            env!("CARGO_MANIFEST_DIR")
+        );
+        let mut log_file = std::fs::File::open(&filename).unwrap();
+        log_file.seek(SeekFrom::Start(8)).unwrap();
+        assert!(log_file.is_ulog());
     }
 }
