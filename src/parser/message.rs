bd30556144f43086d13e5cdab4962f61f8a92ba2
Ilari Mäkimattila <ilari@auterion.com>
2018-11-29 15:03:08 +0100

    Add message header reader

---
+++ src/parser/message.rs
@@ -0,0 +1,123 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::SeekFrom;
+
+use unpack;
+
+const HEADER_SIZE: u64 = 16;
+
+pub trait ULogMessageSource {
+    /// Creates an iterator that reads through every message in the log file
+    ///
+    /// # Examples
+    /// ```
+    /// use std::iter::*;
+    /// use px4_ulog::parser::message::*;
+    ///
+    /// let filename = format!("{}/tests/fixtures/6ba1abc7-b433-4029-b8f5-3b2bb12d3b6c.ulg", env!("CARGO_MANIFEST_DIR"));
+    /// let mut log_file = std::fs::File::open(&filename).unwrap();
+    /// let messages: Vec<ULogMessage> = log_file.messages().collect();
+    /// assert_eq!(messages[0].position(), 19);
+    /// assert_eq!(messages[0].msg_type(), MessageType::FlagBits);
+    /// assert_eq!(messages[0].size(), 40);
+    /// assert_eq!(messages[1].position(), 62);
+    /// assert_eq!(messages[21130].position(), 973045);
+    /// assert_eq!(messages.len(), 21131);
+    /// ```
+    fn messages(&mut self) -> ULogMessageIter;
+}
+
+pub struct ULogMessageIter<'a> {
+    position: u64,
+    file: &'a mut File,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum MessageType {
+    Unknown,
+    Format,
+    Data,
+    Info,
+    MultipleInfo,
+    Parameter,
+    AddLoggedMessage,
+    RemoveLoggedMessage,
+    Sync,
+    Dropout,
+    Logging,
+    FlagBits,
+}
+
+pub struct ULogMessage {
+    msg_type: u8,
+    msg_size: u16,
+    msg_pos: u64,
+}
+
+impl ULogMessage {
+    pub fn msg_type(&self) -> MessageType {
+        match self.msg_type as char {
+            'F' => MessageType::Format,
+            'D' => MessageType::Data,
+            'I' => MessageType::Info,
+            'M' => MessageType::MultipleInfo,
+            'P' => MessageType::Parameter,
+            'A' => MessageType::AddLoggedMessage,
+            'R' => MessageType::RemoveLoggedMessage,
+            'S' => MessageType::Sync,
+            'O' => MessageType::Dropout,
+            'L' => MessageType::Logging,
+            'B' => MessageType::FlagBits,
+            _ => MessageType::Unknown,
+        }
+    }
+
+    pub fn size(&self) -> u16 {
+        self.msg_size
+    }
+
+    pub fn position(&self) -> u64 {
+        self.msg_pos
+    }
+}
+
+impl ULogMessageSource for File {
+    fn messages(&mut self) -> ULogMessageIter {
+        ULogMessageIter {
+            position: HEADER_SIZE,
+            file: self,
+        }
+    }
+}
+
+impl<'a> Iterator for ULogMessageIter<'a> {
+    type Item = ULogMessage;
+
+    fn next(&mut self) -> Option<ULogMessage> {
+        if self.file.seek(SeekFrom::Start(self.position)).is_err() {
+            return None;
+        }
+
+        let mut buffer = [0; 2];
+        if self.file.read_exact(&mut buffer).is_err() {
+            return None;
+        }
+        let msg_size = unpack::as_u16_le(&buffer);
+
+        let mut buffer = [0; 1];
+        if self.file.read_exact(&mut buffer).is_err() {
+            return None;
+        }
+        let msg_type = buffer[0];
+
+        let msg_pos = self.position + 3;
+
+        self.position += msg_size as u64 + 3;
+
+        Some(ULogMessage {
+            msg_type,
+            msg_size,
+            msg_pos,
+        })
+    }
+}
--- src/unpack/mod.rs
+++ src/unpack/mod.rs
@@ -1,6 +1,6 @@
 use std::iter::*;
 
-/// Convert a slice of 8 u8 elements into a u64
+/// Convert a slice of eight u8 elements into a u64
 /// Assumes little endianness.
 ///
 /// # Examples
@@ -13,5 +13,21 @@ pub fn as_u64_le(arr: &[u8; 8]) -> u64 {
     arr.iter()
         .enumerate()
         .map(|(i, v)| (v.clone() as u64) << (8 * i))
+        .sum()
+}
+
+/// Convert a slice of two u8 elements into a u16
+/// Assumes little endianness.
+///
+/// # Examples
+/// ```
+/// use px4_ulog::unpack;
+/// let arr: [u8; 2] = [0, 2];
+/// assert_eq!(unpack::as_u16_le(&arr), 512);
+/// ```
+pub fn as_u16_le(arr: &[u8; 2]) -> u16 {
+    arr.iter()
+        .enumerate()
+        .map(|(i, v)| (v.clone() as u16) << (8 * i))
         .sum()
 }
