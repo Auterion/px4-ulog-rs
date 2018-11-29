2c921e18722b4109bc5bb4f7573ab63b634aa9cb
Ilari Mäkimattila <ilari@auterion.com>
2018-11-29 11:44:27 +0100

    Add file version

--- src/parser/header.rs
+++ src/parser/header.rs
@@ -1,11 +1,12 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::SeekFrom;
+use std::io::{Result, SeekFrom};
 
 const HEADER_BYTES: [u8; 7] = [85, 76, 111, 103, 1, 18, 53];
 
 pub trait ULogHeader {
     fn is_ulog(&mut self) -> bool;
+    fn read_ulog_version(&mut self) -> Result<u8>;
 }
 
 impl ULogHeader for File {
@@ -28,6 +29,23 @@ impl ULogHeader for File {
         } else {
             false
         }
+    }
+
+    /// Extracts the ulog file version
+    ///
+    /// # Examples
+    /// ```
+    /// use px4_ulog::parser::header::*;
+    ///
+    /// let filename = format!("{}/tests/fixtures/6ba1abc7-b433-4029-b8f5-3b2bb12d3b6c.ulg", env!("CARGO_MANIFEST_DIR"));
+    /// let mut log_file = std::fs::File::open(&filename).unwrap();
+    /// assert_eq!(log_file.read_ulog_version().unwrap(), 1);
+    /// ```
+    fn read_ulog_version(&mut self) -> Result<u8> {
+        self.seek(SeekFrom::Start(7))?;
+        let mut buffer = [0; 1];
+        self.read_exact(&mut buffer)?;
+        Ok(buffer[0])
     }
 }
 
@@ -54,5 +72,15 @@ mod tests {
         let mut log_file = std::fs::File::open(&filename).unwrap();
         log_file.seek(SeekFrom::Start(8)).unwrap();
         assert!(log_file.is_ulog());
+    }
+
+    #[test]
+    fn it_fails_to_read_version_from_an_empty_file() {
+        let filename = format!(
+            "{}/tests/fixtures/not_a_log_file.txt",
+            env!("CARGO_MANIFEST_DIR")
+        );
+        let mut log_file = std::fs::File::open(&filename).unwrap();
+        assert!(log_file.read_ulog_version().is_err());
     }
 }
