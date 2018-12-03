b219ee9a06d36df7a44255844cb4196e05bb7de9
Ilari Mäkimattila <ilari@auterion.com>
2018-12-03 13:36:47 +0100

    Trim the dataset field name

--- src/models/data.rs
+++ src/models/data.rs
@@ -12,7 +12,7 @@ pub struct ULogDataIter<'a> {
     data_index: usize,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum DataType {
     UInt64(u64),
     Int32(i32),
@@ -62,6 +62,7 @@ impl<'a> Iterator for ULogDataIter<'a> {
             self.format_index += 1;
             let space = format.find(" ").unwrap();
             let (dtype, fname) = format.split_at(space);
+            let fname = fname.trim();
 
             match dtype {
                 "uint64_t" => {
@@ -121,6 +122,58 @@ impl<'a> Iterator for ULogDataIter<'a> {
                     Some((fname, DataType::Bool(val)))
                 }
                 _ => None,
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use parser::dataset::*;
+    use std::fs::File;
+
+    #[test]
+    fn it_parses_the_data() {
+        let filename = format!(
+            "{}/tests/fixtures/6ba1abc7-b433-4029-b8f5-3b2bb12d3b6c.ulg",
+            env!("CARGO_MANIFEST_DIR")
+        );
+
+        let mut log_file = File::open(&filename).unwrap();
+
+        let first_position = log_file
+            .get_dataset("vehicle_gps_position")
+            .unwrap()
+            .next()
+            .unwrap();
+
+        for (name, data) in first_position.iter() {
+            match name {
+                "timestamp" => assert_eq!(DataType::UInt64(375408345), data),
+                "time_utc_usec" => assert_eq!(DataType::UInt64(0), data),
+                "lat" => assert_eq!(DataType::Int32(407423012), data),
+                "lon" => assert_eq!(DataType::Int32(-741792999), data),
+                "alt" => assert_eq!(DataType::Int32(28495), data),
+                "alt_ellipsoid" => assert_eq!(DataType::Int32(0), data),
+                "s_variance_m_s" => assert_eq!(DataType::Float(0.0), data),
+                "c_variance_rad" => assert_eq!(DataType::Float(0.0), data),
+                "eph" => assert_eq!(DataType::Float(0.29999998), data),
+                "epv" => assert_eq!(DataType::Float(0.39999998), data),
+                "hdop" => assert_eq!(DataType::Float(0.0), data),
+                "vdop" => assert_eq!(DataType::Float(0.0), data),
+                "noise_per_ms" => assert_eq!(DataType::Int32(0), data),
+                "jamming_indicator" => assert_eq!(DataType::Int32(0), data),
+                "vel_m_s" => assert_eq!(DataType::Float(0.0), data),
+                "vel_n_m_s" => assert_eq!(DataType::Float(0.0), data),
+                "vel_e_m_s" => assert_eq!(DataType::Float(0.0), data),
+                "vel_d_m_s" => assert_eq!(DataType::Float(0.0), data),
+                "cog_rad" => assert_eq!(DataType::Float(0.0), data),
+                "timestamp_time_relative" => assert_eq!(DataType::Int32(0), data),
+                "fix_type" => assert_eq!(DataType::UInt8(3), data),
+                "vel_ned_valid" => assert_eq!(DataType::Bool(false), data),
+                "satellites_used" => assert_eq!(DataType::UInt8(10), data),
+                x => panic!(format!("unexpected field '{}'", x)),
             }
         }
     }
